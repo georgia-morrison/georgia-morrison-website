0ad6d26d5cf88b715e542f31525e50fd3d68efc5
Georgia Morrison <87599176+georgia-morrison@users.noreply.github.com>
2025-11-23 15:16:14 +0000

    vite config path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:'/georgia-morrison-portfolio/',
   server: {
     host: "::",
     port: 8080,
